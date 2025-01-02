5014a6fdb39968254fd23a60e4b83d3d8b1ecb4c
abhishek kumar <31090188+abhi00o7@users.noreply.github.com>
2025-01-02 08:08:05 +0530

    bug fix file name update file name

--- DAY1/multi_folder.py
+++ DAY1/multi_folder.py
@@ -2,8 +2,9 @@ import os
 
 # Define the base directory where folders will be created
 base_dir = 'C:/Users/abhis/Projects/100DaysOfCode2025/'
-dir_count = 102
+dir_count = 100
 folder_name = 'DAY'
+
 # Create folders for as many days as you want
 
 
@@ -11,11 +12,12 @@ def create_folder(base_dir, count, folder_name):
     print(f'Creating {count} folders in {base_dir} with name {folder_name}')
     for index in range(1, count+1):
         print(f'Creating folder: {folder_name + str(index)}')
-        folder_name = folder_name + str(index)
-        folder_path = os.path.join(base_dir, folder_name)
+        f_name = folder_name + str(index)
+        folder_path = os.path.join(base_dir, f_name)
         os.makedirs(folder_path, exist_ok=True)
         print(f'Created folder: {folder_path}')
 
 
 # Call the function with the base directory and number of days
 create_folder(base_dir, dir_count, folder_name)
+
